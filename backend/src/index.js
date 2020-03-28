65563f97bfba458dba7e0535c74f524b21e4aef4
Adriano Moreira <adrianodesenv@gmail.com>
2020-03-27 23:43:01 -0300

    Insert the FrontEnd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const routes = require("./routes");
 const app = express();
 
@@ -10,5 +10,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-
-app.listen(3333);
+app.listen(3334);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,22 @@
-const express = require('express');
+const express = require("express");
+
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
 const ProfileController = require("./controllers/ProfileController");
 
 const SessionController = require("./controllers/SessionController");
- 
 
 const router = express.Router();
 
- router.get("/ongs", OngController.index);
- router.post("/ongs", OngController.create);
+router.get("/ongs", OngController.index);
+router.post("/ongs", OngController.create);
 
+router.post("/sessions", SessionController.create);
 
- router.post("/sessions", SessionController.create);
+router.get("/profiles", ProfileController.index);
 
+router.get("/incidents", IncidentController.index);
+router.post("/incidents", IncidentController.create);
+router.delete("/incidents/:id", IncidentController.delete);
 
- router.get("/profiles", ProfileController.index);
-
-
- router.get("/incidents", IncidentController.index);
- router.post("/incidents", IncidentController.create);
- router.delete("/incidents/:id",IncidentController.delete);
-
-module.exports = router
+module.exports = router;
